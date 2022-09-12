42adc9cff5d378b7baf454a6fb0f5d5d7a3f4cb6
Mbzneto <mbzneto@gmail.com>
2022-09-12 12:11:17 -0300

    Correções Música

--- Assets/Scripts/DoorController.cs
+++ Assets/Scripts/DoorController.cs
@@ -5,7 +5,8 @@ using UnityEngine.SceneManagement;
 
 public class DoorController : MonoBehaviour
 {
-
+    private GameObject musicObj; 
+    private AudioSource audio;
     // Start is called before the first frame update
     void Start()
     {
@@ -22,7 +23,16 @@ public class DoorController : MonoBehaviour
         if (other.gameObject.CompareTag("Player")) {
             int proxLvl = PlayerPrefs.GetInt("Level") + 1;
             PlayerPrefs.SetInt("Level", proxLvl);
+            // stopMusic();
             SceneManager.LoadScene("Map");
         }
+    }
+
+    private void stopMusic()
+    {
+        musicObj = GameObject.Find("AudioSource");
+        audio = musicObj.GetComponent<AudioSource>();
+        // Debug.Log(musicObj.GetComponent<AudioSource>().clip);
+        GetComponent<AudioSource>().Stop();
     }
 }
